9b4b2cdcf35fe371ca132eda0cc0a41890e89896
Pranay Karani <pranaykarani@outlook.com>
2020-03-28 11:43:41 +0530

    - basic project setup - views & entities setup

---
+++ app/src/main/java/com/reach52/entities/User.kt
@@ -0,0 +1,11 @@
+package com.reach52.entities
+
+data class User(
+
+    val name: String,
+    val dob: Long,
+    val age: Int,
+    val address: String,
+    val image: ByteArray
+
+)
---
+++ app/src/main/java/com/reach52/views/AddActivity.kt
@@ -0,0 +1,13 @@
+package com.reach52.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.reach52.R
+
+class AddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+    }
+}
